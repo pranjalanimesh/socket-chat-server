bccfcb822b5bac34de30a7beccfa2f8eaa785745
Ashwanidubeyiitb <134732136+Ashwanidubeyiitb@users.noreply.github.com>
2023-12-10 12:43:20 +0530

    Add files via upload
    These are of backend

--- Chat.js
+++ Chat.js
@@ -1,104 +1,20 @@
-// SingleChat.js
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import io from 'socket.io-client';
-import Page from 'src/components/Page';
-import {
-  Typography,
-  Container,
-  Box,
-  Grid,
-  Button,
-  Divider,
-  TextField
-} from '@mui/material';
+const mongoose = require("mongoose");
 
-const Chat = ({
-  selectedChat,
-  user,
-  allMessage,
-  setAllMessage,
-  sendMessage
-}) => {
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    groupAdmin: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
 
-  const [newMessage, setNewMessage] = useState('');
+const Chat = mongoose.model("Chat", chatModel);
 
-  const handleTextFieldChange = (e) => {
-    setNewMessage(e.target.value);
-  };
-
-  const handleEnterPress = (e) => {
-    if (e.keyCode == 13) {
-      sendMessageChat();
-    }
-  }
-
-  const sendMessageChat = () => {
-    try {
-      if (newMessage.trim() === '') {
-        setNewMessage('');
-        return;
-      }
-
-      const data = {
-        'content': newMessage,
-        'sender': user.email,
-        'receiver': selectedChat.email,
-        'senderId': user.id,
-        'receiverId': selectedChat.id,
-        'timestamp': Date.now() // Add timestamp here
-      };
-
-      allMessage[selectedChat.email] = [...allMessage[selectedChat.email], data];
-      sendMessage(data);
-      setAllMessage(allMessage);
-      setNewMessage('');
-    } catch (error) {
-      console.error('Error sending message:', error.message);
-    }
-  };
-
-  if (selectedChat.email === 'default') {
-    return (
-      <Container>
-        <Typography variant="h4" color='black'>Default Chat</Typography>
-        <Divider />
-      </Container>
-    )
-  }
-
-  return (
-    <Container>
-      <Typography variant="h4" sx={{ color: "black" }}>{selectedChat.name}</Typography>
-      <Divider />
-      <Box sx={{ height: 500, background: 'yellow' }}>
-        {allMessage[selectedChat.email].map((message, index) =>
-          (
-            <div key={index} style={{ textAlign: message.sender === user.email ? 'right' : 'left', color: 'black' }}>
-              {message.content}
-              <br />
-              <span style={{ fontSize: '0.7em' }}>
-                {message.sender === user.email ? 'You' : selectedChat.name}</span>
-              <br />
-              {/* Display the timestamp */}
-              <span style={{ fontSize: '0.7em' }}>Sent at: {new Date(message.timestamp).toLocaleString()}</span>
-            </div>
-          ))}
-      </Box>
-
-      <Box>
-        <TextField
-          id="outlined-basic"
-          variant="filled"
-          sx={{ width: 630, alignContent: "flex-end" }}
-          value={newMessage}
-          onChange={handleTextFieldChange}
-          onKeyDown={handleEnterPress} />
-
-        <Button onClick={sendMessageChat}>Send</Button>
-      </Box>
-    </Container>
-  );
-};
-
-export default Chat;
+module.exports = Chat;
